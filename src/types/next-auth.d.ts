40c208c96835de6e97b148edc7582d51f14f5f35
Your Name <you@example.com>
2026-03-10 22:26:47 +0200

    fix: add role type to next-auth session

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,4 +1,4 @@
-import "next-auth"
+import NextAuth from "next-auth"
 
 declare module "next-auth" {
   interface Session {
@@ -7,6 +7,19 @@ declare module "next-auth" {
       name?: string | null
       email?: string | null
       image?: string | null
+      role: string        // ✅ لازم يكون موجود
     }
+  }
+
+  interface User {
+    id: string
+    role: string          // ✅ لازم يكون موجود
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string
+    role: string          // ✅ لازم يكون موجود
   }
 }
